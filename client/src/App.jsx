8de0991626c1901f0527aa4afae8be390aa9a218
Jeff Williams <88175879+orphandeity@users.noreply.github.com>
2023-10-12 14:03:55 -0700

    adds Layout component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
+import Layout from './routes/Layout'
+
 const queryClient = new QueryClient()
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>hello world!</div>,
+    element: <Layout />,
   },
 ])
 
---
+++ client/src/routes/Layout.jsx
@@ -0,0 +1,39 @@
+import { Outlet } from 'react-router-dom'
+
+let style = {
+  wrapper: {
+    display: 'flex',
+    flexFlow: 'column nowrap',
+    minHeight: '100vh',
+    padding: 'var(--padding)',
+  },
+  header: {
+    flex: '0 0 auto',
+    paddingBottom: 'var(--padding)',
+  },
+  main: {
+    flex: '1 1 auto',
+  },
+  footer: {
+    textAlign: 'center',
+    paddingTop: 'var(--padding)',
+  },
+}
+
+function Layout() {
+  return (
+    <div style={style.wrapper}>
+      <header style={style.header}>
+        <b>express-vite</b>
+      </header>
+      <div style={style.main}>
+        <Outlet />
+      </div>
+      <footer style={style.footer}>
+        <small>🍄 created by orphandeity</small>
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
