b7c779d3ef41a8a3fae094accc5c117f083cb9ee
Jeff Williams <88175879+orphandeity@users.noreply.github.com>
2023-10-12 01:03:04 -0700

    adds vite proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd(), '')
+  if (command === 'serve') {
+    return {
+      // dev specific congig
+      plugins: [react()],
+
+      server: {
+        proxy: {
+          '/api': `http://localhost:${env.EXPRESS_PORT ?? 2300}`,
+        },
+      },
+    }
+  } else {
+    return {
+      // build specific config
+      plugins: [react()],
+    }
+  }
 })
