a2bbf9447004ed97069ddc288b79684329c17aee
Jeff Williams <88175879+orphandeity@users.noreply.github.com>
2023-10-13 00:09:38 -0700

    adds home page styles

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -10,23 +10,25 @@ const getMessage = async () => {
 }
 
 function Home() {
-  const { data } = useQuery(['message'], getMessage)
+  const { data, isLoading, isError } = useQuery(['message'], getMessage)
 
-  const message = data ? (
-    <strong>😎 {data.message}</strong>
-  ) : (
+  const message = isLoading ? (
+    <strong>🙄 loading...</strong>
+  ) : isError ? (
     <strong>🤬 something went wrong.</strong>
+  ) : (
+    <strong>😎 {data.message}</strong>
   )
 
   return (
     <div style={{ display: 'grid', gap: 'var(--padding)' }}>
       <h1>Welcome!</h1>
-      <p>
+      <p style={{ maxWidth: '550px' }}>
         This template combines Express and React to help you create dynamic web
         applications effortlessly. If everything is set up correctly, you should
-        see a friendly message from the server. Enjoy your development journey!
-        ✌️
+        see a friendly message from the server below.
       </p>
+      <p>Enjoy your development journey! ✌️</p>
       <p>{message}</p>
     </div>
   )
