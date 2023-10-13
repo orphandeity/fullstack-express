0ed3b1240c7156dd386eed31de24bfa55e933836
Jeff Williams <88175879+orphandeity@users.noreply.github.com>
2023-10-13 14:03:34 -0700

    adds api/auth/users endpoint

--- server/services/auth.js
+++ server/services/auth.js
@@ -29,6 +29,14 @@ class AuthService {
       return done(err)
     }
   }
+
+  async userList() {
+    try {
+      return await UserModel.getUsers()
+    } catch (err) {
+      throw new Error(err)
+    }
+  }
 }
 
 module.exports = new AuthService()
