fdead6fe6953021490c044466e89f6ba704f9546
Jeff Williams <88175879+orphandeity@users.noreply.github.com>
2023-10-14 21:02:35 -0700

    adds validation middleware

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,26 +1,40 @@
 const express = require('express')
-const router = express.Router()
+const { body } = require('express-validator')
 const { authenticate, validate } = require('../middleware')
-
 const AuthService = require('../services/auth')
+
+const router = express.Router()
 
 module.exports = (app, passport) => {
   app.use('/api/auth', router)
 
-  router.post('/register', async (req, res, next) => {
-    try {
-      let user = await AuthService.registerUser(req.body)
-      req.login(user, (err) => {
-        if (err) return next(err)
-        return res.status(200).json(user)
-      })
-    } catch (err) {
-      next(err)
+  router.post(
+    '/register',
+    [
+      body('username').trim().notEmpty().isAlphanumeric(),
+      body('password').trim().notEmpty(),
+      validate,
+    ],
+    async (req, res, next) => {
+      try {
+        let user = await AuthService.registerUser(req.body)
+        req.login(user, (err) => {
+          if (err) return next(err)
+          return res.status(200).json(user)
+        })
+      } catch (err) {
+        next(err)
+      }
     }
-  })
+  )
 
   router.post(
     '/login',
+    [
+      body('username').trim().notEmpty().isAlphanumeric(),
+      body('password').trim().notEmpty(),
+      validate,
+    ],
     passport.authenticate('local', {
       successMessage: true,
       failureMessage: true,
